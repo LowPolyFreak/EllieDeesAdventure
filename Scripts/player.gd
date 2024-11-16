2c5df8f2cdc40d9dad3286283ebf502d16c7825f
LowPolyFreak <grenierlowpoly@gmail.com>
2024-11-15 21:14:12 -0500

    Removed jump

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -83,8 +83,8 @@ func _physics_process(delta):
 
 	# Handle jump.
 	
-	if Input.is_action_just_pressed(jump) and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed(jump) and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 	
 	var input_dir = Input.get_vector(left, right, up, down)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()

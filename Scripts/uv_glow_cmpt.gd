370ceb00bbbbf0358d714a74af564e9362e91475
LowPolyFreak <grenierlowpoly@gmail.com>
2024-11-17 12:28:36 -0500

    Polished UV glow

--- Scripts/uv_glow_cmpt.gd
+++ Scripts/uv_glow_cmpt.gd
@@ -2,7 +2,7 @@ extends Area3D
 
 @export var mesh_target: MeshInstance3D
 @export_range(0.0, 1.0) var glow_amount = 0.75
-@export_range(0.01, 1.0) var fade_out_speed = 0.15
+@export_range(0.01, 1.0) var fade_out_speed = 0.2
 
 @onready var dee = get_tree().get_first_node_in_group("Player_2") as Player
 @onready var material: StandardMaterial3D
@@ -11,16 +11,20 @@ var occupied: bool
 var tween: Tween
 
 func _ready() -> void:
-	$DetectionRadius.shape.radius = dee.glow_light_range
+	$DetectionRadius.shape.radius = dee.glow_light_range - 2
 	material = mesh_target.get_active_material(0).duplicate()
 	mesh_target.set_surface_override_material(0, material)
 	material.albedo_color.a = 0
 
 func _process(delta: float) -> void:
-	if occupied and dee.glowing:
-		material.albedo_color.a = lerpf(material.albedo_color.a, glow_amount, 1.5 * delta)
-	else:
-		material.albedo_color.a = lerpf(material.albedo_color.a, 0, fade_out_speed * delta)
+	if dee:
+		if occupied and dee.glowing:
+			material.albedo_color.a = lerpf(material.albedo_color.a, glow_amount, 1.5 * delta)
+		else:
+			if material.albedo_color.a <= 0.05:
+				material.albedo_color.a = 0
+			else:
+				material.albedo_color.a = lerpf(material.albedo_color.a, 0, fade_out_speed * delta)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
@@ -29,4 +33,3 @@ func _on_body_entered(body: Node3D) -> void:
 func _on_body_exited(body: Node3D) -> void:
 	if body is Player:
 		occupied = false
-	
